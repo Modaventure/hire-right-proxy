39135d0583354bd693617b4b17dd52e62c5c9082
Delian Mitankin <dmitankin@obecto.com>
2015-07-28 12:07:51 +0300

    Restore a missing Serializable implementation

--- src/main/java/com/mv/hr/dto/CVAnalysisExtendedRequestDataDTO.java
+++ src/main/java/com/mv/hr/dto/CVAnalysisExtendedRequestDataDTO.java
@@ -1,6 +1,8 @@
 package com.mv.hr.dto;
 
-public class CVAnalysisExtendedRequestDataDTO {
+import java.io.Serializable;
+
+public class CVAnalysisExtendedRequestDataDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	public DocumentDTO CVDocument;
